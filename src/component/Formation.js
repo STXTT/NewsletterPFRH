5b301854b736f938167acef3d7627e2f13fb4ad8
psamaran <pierre.samaran@etu.unistra.fr>
2022-07-04 11:34:13 +0200

    Add comment

--- src/component/Formation.js
+++ src/component/Formation.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+// Ce composant permet de gérer l'affichage d'une formation 
 const Formation = ({formation}) => {
+
+  //Si les caractères de la date sont la balise <p> c'est qu'il n'y a pas de date, la formation à donc été férmé et ne doit pas être affiché
   if(!formation.date.includes("<p>"))
     return(
 <div style={{border : "solid black 2px ", margin : "2px", marginTop : "8px"}}>
--- src/component/Newsletter.js
+++ src/component/Newsletter.js
@@ -7,21 +7,25 @@ import 'react-quill/dist/quill.snow.css';
 
 
 const Newsletter = ({selectedFormation}) => {
+  //variable contenant le code html du mail, initialisé avec un message d'introduction
   const [value, setValue] = useState('<p>Bonjour,<br/>La PFRH vous informe des formations suivantes. </p>');
   
   var outro = "Merci par avance d'en assurer la diffusion <br/> Bien cordialement,"
- 
+
+  //A chaque fois que l'utilisateur choisi une formation elle est ajouté à la suite du contenu de value.
 useEffect(()=>{
-  setValue(value+"<strong><br/>"+selectedFormation?.title+"</strong> : "+selectedFormation?.domain+"<br/>Date : "+selectedFormation?.date+" Lieu : "+selectedFormation?.address+"<br/>"+`<a href=${selectedFormation?.link.slice(0,selectedFormation?.link.lastIndexOf("&"))}>Voir sur Safire</a>`)
+  setValue(value+"<strong><br/><p>"+selectedFormation?.title+"</strong> : "+selectedFormation?.domain+"<br/>Date : "+selectedFormation?.date+" Lieu : "+selectedFormation?.address+`<a href=${selectedFormation?.link.slice(0,selectedFormation?.link.lastIndexOf("&"))}>Voir sur Safire</a></p>`)
 },[selectedFormation])
 
     return(
 <div>
-{<ReactQuill style={{marginTop : "-5%"}} theme="snow" value={value} onChange={setValue}/>}
+  
+{//ReactQuill est un éditeur pour reactJS
+<ReactQuill style={{marginTop : "-5%"}} theme="snow" value={value} onChange={setValue}/>}
 
 
 
-<ButtonMailto label="Envoyer" mailto={"mailto:no-reply@example.com?html-body="+value+outro+"&subject=Fçormations à venir sur safire"} />
+<ButtonMailto label="Envoyer" mailto={'mailto:no-reply@example.com?html-body='+value+outro+"&subject=Formations à venir sur safire"} />
 
 
 </div>
